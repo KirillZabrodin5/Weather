4eeaabd6d82d7d20260bbb866318706bb343cf83
KirillZabrodin5 <fea.z@yandex.ru>
2024-06-13 23:06:06 +0500

    started adding templates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,45 @@
-import entity.Location;
-import entity.User;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
-import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(Location.class);
-        configuration.configure();
+        var resolver = new ClassLoaderTemplateResolver();
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
 
-        try(SessionFactory sessionFactory = configuration.buildSessionFactory();
-            Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
+        var context = new Context();
+        context.setVariable("name", "Bunny");
+        context.setVariable("date", LocalDate.now().toString());
 
-            Location location = Location.builder()
-                    .name("Moscow")
-                    .userId(1)
-                    .latitude(new BigDecimal("10.50"))
-                    .longitude(new BigDecimal("11.7"))
-                    .build();
-            session.persist(location);
-            session.getTransaction().commit();
-        }
+        var templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(resolver);
+
+        var result = templateEngine.process("index", context);
+        System.out.println(result);
+        //TODO: код для добавления в БД сущности
+//        Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(User.class);
+//        configuration.addAnnotatedClass(Location.class);
+//        configuration.configure();
+//
+//        try(SessionFactory sessionFactory = configuration.buildSessionFactory();
+//            Session session = sessionFactory.openSession()) {
+//            session.beginTransaction();
+//
+//            Location location = Location.builder()
+//                    .name("Moscow")
+//                    .userId(1)
+//                    .latitude(new BigDecimal("10.50"))
+//                    .longitude(new BigDecimal("11.7"))
+//                    .build();
+//            session.persist(location);
+//            session.getTransaction().commit();
+//        }
     }
 }
---
+++ src/main/java/servlet/AuthServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class AuthServlet extends HttpServlet {
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() {
+        ClassLoaderTemplateResolver classLoaderTemplateResolver = new ClassLoaderTemplateResolver();
+        classLoaderTemplateResolver.setTemplateMode("HTML");
+        classLoaderTemplateResolver.setPrefix("templates/");
+        classLoaderTemplateResolver.setSuffix(".html");
+
+        templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(classLoaderTemplateResolver);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
